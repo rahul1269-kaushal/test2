7826e4c93e49d0e6f04043c775117f8fdfaf7bbd
rahul1269-kaushal <59966780+rahul1269-kaushal@users.noreply.github.com>
2024-01-15 20:32:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
+
+from DB_operations import add_user
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/')
+def hello():
+    return render_template("/index.html")
+
+
+@app.route('/users', methods=['POST', 'GET'])
+def user():
+    if request.method == "POST":
+        email = request.form["email"]
+        psw = request.form["psw"]
+        psw_repeat = request.form["psw_repeat"]
+
+        # saving all the values to db
+        add_new = add_user(email, psw, psw_repeat)
+        return redirect(url_for('helloworld'))
+    else:
+        return render_template('index.html')
